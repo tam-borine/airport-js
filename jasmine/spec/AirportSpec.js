91fcb6fff79b9a01566f59e030282f6e49dfc141
BenRoss92 <benjaminjackross@gmail.com>
2016-09-13 15:25:20 +0100

    airport capacity set

--- jasmine/spec/AirportSpec.js
+++ jasmine/spec/AirportSpec.js
@@ -23,4 +23,21 @@ describe('Airport', function() {
     expect(airport._planes).toEqual([]);
   });
 
+  it("has a default capacity", function() {
+    expect(airport._CAPACITY).not.toBeUndefined();
+  })
+
+  it("capacity can be set", function() {
+    airport.setCapacity(20);
+    expect(airport._CAPACITY).toEqual(20);
+  })
+
+  it("cannot hold more planes than capacity", function() {
+    var i = 0;
+    while (i <= airport._CAPACITY) {
+      airport.receivePlane(plane)
+    };
+    expect(function() {airport.receivePlane(plane);}).toThrow(new Error ("Capacity reached!"));
+  })
+
 });
--- jasmine/src/Airport.js
+++ jasmine/src/Airport.js
@@ -1,5 +1,6 @@
 function Airport() {
   this._planes = [];
+  this._CAPACITY = 10;
 
 }
 
@@ -8,10 +9,14 @@ Airport.prototype = {
   receivePlane: function(plane) {
     this._planes.push(plane);
   },
-  
+
   ejectPlane: function(plane) {
     var index = this._planes.indexOf(plane);
     this._planes.splice(index, 1);
+  },
+
+  setCapacity: function(number) {
+    this._CAPACITY = number;
   }
 
 };
